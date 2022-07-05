0c50513b3c8061f8fa4ef0f08a8bba7dd3d6d03f
Emre Eryiğit <emreeryigit@Emre-MacBook-Air.local>
2022-07-05 19:52:53 +0300

    refactor done

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "my name", age: 2})
-
+const user = new User({ name: "my name", age: 1})
+user.on("change", () => {
+    console.log("user was changed");
+    
+})
+user.set({name: "EMREMRe"})
--- src/models/User.ts
+++ src/models/User.ts
@@ -12,5 +12,17 @@ export interface UserProps {
 export class User {
     events: Eventing = new Eventing()
     sync: Sync<UserProps> = new Sync<UserProps>("http://localhost:3000/users")
-    attributes: Attributes<UserProps> = new Attributes<UserProps>()
+    attributes: Attributes<UserProps>
+    constructor(attrs: UserProps) {
+        this.attributes = new Attributes(attrs)
+    }
+
+    get on() { return this.events.on };
+    get trigger() {return this.events.trigger}
+    get get() { return this.attributes.get};
+
+    set(update: UserProps): void {
+        this.attributes.set(update)
+        this.events.trigger("change")
+    }
 }
