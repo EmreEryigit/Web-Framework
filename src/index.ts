4f6071a072204dbe005ed8f3f55f30f13ae2aa95
Emre Eryiğit <emreeryigit@Emre-MacBook-Air.local>
2022-07-05 22:02:14 +0300

    Userform completed.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { Collection } from "./models/Collection";
 import { User, UserProps } from "./models/User";
+import { UserForm } from "./views/UserForm";
 
+const user = User.buildUser({name: "Emre", age: 25})
+const userForm = new UserForm(
+    document.getElementById("root")!,
+    user
+)
+userForm.render()
 const collection = User.buildUserCollection()
 
 collection.on("change", () => {
