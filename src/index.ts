fc45eaee878644830917e5b856e6af6bd4aa1894
Emre Eryiğit <emreeryigit@Emre-MacBook-Air.local>
2022-07-05 23:29:03 +0300

    Completed.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { Collection } from "./models/Collection";
 import { User, UserProps } from "./models/User";
-import { UserEdit } from "./views/UserEdit";
-import { UserForm } from "./views/UserForm";
+import { UserList } from "./views/UserList";
 
-const user = User.buildUser({name: "Emre", age: 25})
-const root = document.getElementById("root")!
-const userEdit = new UserEdit(root, user)
-userEdit.render()
-console.log(userEdit);
 
+const users = new Collection("http://localhost:3000/users", (json: UserProps) => {
+    return User.buildUser(json)
+})
+
+users.on("change", () => {
+    const root = document.getElementById("root")!
+    new UserList(root, users).render()
+})
+
+users.fetch()
